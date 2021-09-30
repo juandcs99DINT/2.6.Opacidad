566d4388c8ff37347023c99db496988b35cd13c4
Juan Diego <jd.cabrera@iesdoctorbalmis.com>
2021-09-30 14:55:49 +0200

    Agregar archivos de proyecto.

---
+++ Opacidad/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opacidad
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CambiarOpacity_mouseEnter(object sender, RoutedEventArgs e)
+        {
+            Image imagen = (Image)sender;
+            imagen.Opacity = 1;
+        }
+
+        private void CambiarOpacity_mouseDown(object sender, RoutedEventArgs e)
+        {
+            Image imagen = (Image)sender;
+            imagen.Opacity = 0.5;
+        }
+
+    }
+}
